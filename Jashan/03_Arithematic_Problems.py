e9953ee34631182720f9ab08333a64fd851acd5c
Coding Hashira <codinghashira@gmail.com>
2022-01-03 18:37:36 +0530

    Organised code and completed 3/3 challenges of the day!

---
+++ Jashan/03_Arithematic_Problems.py
@@ -0,0 +1,18 @@
+# Made by Jashan(Tanjiro)
+# -------------Challenge-------------
+
+# The provided code stub reads two integers from STDIN a and b. Add code to print three lines where:
+    # The first line contains the sum of the two numbers.
+    # The second line contains the difference of the two numbers (first - second).
+    # The third line contains the product of the two numbers.
+
+# -------------Code-------------
+
+# taking input
+a=int(input())
+b=int(input())
+
+# printing
+print(a+b)
+print(a-b)
+print(a*b)
