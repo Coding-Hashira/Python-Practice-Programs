af1a63c92a722ca9867b8eafd46112c7e18b30e1
Aman Bahl <85411047+aman-bahl@users.noreply.github.com>
2022-01-04 17:27:13 +0530

    Update cube-of-a-number.py

--- Aman/cube-of-a-number.py
+++ Aman/cube-of-a-number.py
@@ -1,1 +1,12 @@
+# Made by Aman
+# -------------Challenge-------------
+# Write a code in Python to take input from a user and print the cube of that number.
 
+n = int(input("\nPlease enter a number :\n"))
+
+def cbrt(n):
+    return n** 3
+
+cube1 = cbrt(n)
+
+print("Answer: Cube of {0} is {1}".format(n, cube1))
