6d551d5f68c1cd6542788260d7ce288a457475ef
Coding Hashira <codinghashira@gmail.com>
2022-01-04 19:18:57 +0530

    6/6

---
+++ Jashan/06_Area_Of_Circle.py
@@ -0,0 +1,11 @@
+# Made by Jashan(Tanjiro)
+# -------------Challenge-------------
+# Make A Program To calculate area of a circle
+# Source: my mind
+
+# -------------Code-------------
+while True:
+    pie = 3.14
+    r = float(input('Radius:\n'))
+    area=pie*r*r
+    print('Area:\n'+str(area))
