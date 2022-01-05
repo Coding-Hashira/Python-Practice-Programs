dbfc8ed12e374d5005e99fa2f400c9e6f13f5748
TalklessKush <90184217+TalklessKush@users.noreply.github.com>
2022-01-05 17:18:04 +0530

    Add files via upload

---
+++ Kush/05_Create_a_Pattern.py
@@ -0,0 +1,9 @@
+# Made by Kush(TalklessKush) 
+
+#Challenge - Write a python function to print first n lines of the pattern
+
+#Code-
+n = 3 
+for i in range(n): #Creating a for loop to print the pattern
+	print("*" * (n-i)) #Prints * n-i times
+
