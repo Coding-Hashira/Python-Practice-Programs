6ddaac8be752968ae88e8939e1b388ebbfc05ef1
Aman Bahl <85411047+aman-bahl@users.noreply.github.com>
2022-01-05 20:55:00 +0530

    Create 05_bmi-calculator.py

---
+++ Aman/05_bmi-calculator.py
@@ -0,0 +1,25 @@
+bmi = weight / sq(height)
+
+
+if bmi < 18.5:
+    
+    print("\n--------------------------------------\n\nAnswer:\n")
+    print("You're Under weight\n")
+
+
+elif bmi >= 18.5 and bmi <= 24.9:
+
+    print("\n--------------------------------------\n\nAnswer:\n")
+    print("You're normal\n")
+
+
+elif bmi >= 25 and bmi <= 29.9:
+
+    print("\n--------------------------------------\n\nAnswer:\n")
+    print("Overweight\n")
+
+
+elif bmi >= 30.0:
+
+    print("\n--------------------------------------\n\nAnswer:\n")
+    print("You're Obese\n")
