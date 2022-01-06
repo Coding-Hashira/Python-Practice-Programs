fc0675956a28ef8b058a787eb2a659b06fa9f7cc
Aman Bahl <85411047+aman-bahl@users.noreply.github.com>
2022-01-06 11:08:23 +0530

    Create positive-negative.py

---
+++ Aman/positive-negative.py
@@ -0,0 +1,13 @@
+n = int(input("\nPlease enter the number: \n\n"))
+
+if n == 0:
+    
+    print("\nZero\n")
+    
+elif n >= 0:
+    
+    print("\nPositive\n")
+    
+else:
+    
+    print("\nNegative\n")
